597696604416f401454f95054ee7c0014d53ba1c
Vincent Franco <vince@freshivore.net>
2013-11-20 01:08:47 -0800

    Wrapping everything but shift strobe.

--- tiny85/main.c
+++ tiny85/main.c
@@ -1,189 +1,180 @@
-#include <stdio.h>
-#include <math.h>
+/*
+ * Timers:
+ *
+ * Like all digital systems a timer requires a clock to run.
+ * As each clock pulse increments the timer's counter by one,
+ * The time measures the intervals in perious of one on the input frequency.
+ *
+ */
 
-// Include all AVR headers
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/pgmspace.h>
-#include <inttypes.h>
-
 #include <util/delay.h>
 
-#define PWM_R OCR0A
-#define PWM_G OCR0B
+volatile uint16_t clock    = 0;
+volatile uint8_t  tick     = 0;
+volatile uint8_t  running  = 0;
 
-// Mocking voltage input
-#define POWER_MIN 1
-#define POWER_MAX 1024
-#define SHIFT_AT  5
+/*
+ * LED Pins used as tach.
+ */
+#define LED_G     OCR1A
+#define LED_R     OCR1B
 
-#define MAX_LUM             255
-#define TOTAL_RANGE MAX_LUM * 3
-#define ASC                   1
-#define DESC                 -1
+/*
+ * Max value for set_intensity()
+ */
+#define INTENSITY 255
 
-typedef struct
+/*
+ * Idle    1,000 RPM =  16.6667Hz
+ * Redline 8,500 RPM = 141.6667Hz
+*/
+#define IDLE      16
+#define REDLINE   141
+
+/*
+ * Our clock count at no-prescale with register overflow
+ * 31250 = 8MHz / 256
+ */
+#define MAX_CLOCK 31250
+
+ISR( TIMER0_OVF_vect )
 {
-  unsigned int red;
-  unsigned int green;
-  unsigned int blue;
-} ColorRGB; // Defines color space
+  clock++;
+}
 
-void     power_to_luminance   ( uint16_t power , ColorRGB * _rgb );
-uint16_t power_for            ( uint16_t power );
-void     run_startup_sequence ( ColorRGB * _initRGB );
-void     set_flash            ( int toggle );
-void     set_pwm_intensities  ( ColorRGB * __rgb );
+ISR ( INT0_vect )
+{
+  running = 1;
+  tick++;
+}
 
-int main (void)
+/*
+ * Setup timer counter register 1 as PWM
+ * drives red and green anodes of RGB LED.
+ */
+void counter1_pwm_init ( void )
+{
+  TCCR1 |= (1<<PWM1A) | (1<<COM1A0) | (1<<CS10); // Enable PWM, no prescaler.
+  GTCCR |= (1<<PWM1B) | (2<<COM1B0);             // Enable use of OC1B, clear OC1B on compare-match, set at BOTTOM
+}
+
+/*
+ * Enables falled edge pin change interrupt for ignition pulse counting.
+ * from low voltage side of ignition coil.
+ */
+void int0_pin_change_init ( void )
+{
+  MCUCR |= (1<<ISC01); // Interrupt on falling edge of INT0.
+  GIMSK |= (1<<INT0);  // INT0 Interrupt request 0 enable.
+}
+
+/*
+ * Enables clock cycle counting to derive RPM from clock/ignition ticks.
+ */
+void timer0_init ( void )
+{
+  TIMSK  |= (1<<TOIE0);  // Overflow Interrupt enable.
+  TCCR0B |= (1<<CS00);   // Set clk/I/O/ No prescaling.
+}
+
+void set_intensity ( uint8_t range )
+{
+  LED_G = INTENSITY - range;
+  LED_R = range;
+}
+
+void set_output_pins ( void )
+{
+  DDRB |= (1<<DDB4) | (1<<DDB2) | (1<<DDB1); // Set PB4, PB2 PB1 as output.
+}
+
+void startup_routine ( void )
 {
 
-  /* - Attiny85 Setup ------------------------------------------------------ */
-
-  DDRB |= (1<<DDB2) (1<<DDB1) | (1<<DDB0); // Set pins 3, (2,1) as output.
-
-  // PWM -----------------------------------------------------------------------
-  TCCR0A = (1<<COM0A1) | (1<<COM0A0); // Non inverting mode (OC0A and OC0B)
-  TCCR0A |= (1<<WGM01) | (1<<WGM00); // Fast PWM
-  TCCR0B = (1<<CS00); // No prescaling
+  uint8_t start     = 0;
+  uint8_t range     = 1;
+  uint8_t direction = 1;
 
 
-  ColorRGB rgb = {.red = 0, .green = 0, .blue = 0}; // Holds intensity values.
+  for (; start < 255; ++start)
+  {
+    LED_G = start;
+    _delay_ms(5);
+  }
 
-  int toggle = 1;
-  int adc_value; // Stores ADC conversion.
+  while (range > 0)
+  {
 
-  run_startup_sequence(&rgb);
+    if ( range >= INTENSITY )
+    {
+      direction = -1;
+    }
 
-  /*
-   * Program loop
-   */
+    range += direction;
+    set_intensity(range);
+    _delay_ms(3);
+  }
+
+  LED_G = 0;
+  _delay_ms(80);
+  LED_G = 255;
+  _delay_ms(80);
+  LED_G = 0;
+  _delay_ms(80);
+  LED_G = 255;
+  _delay_ms(80);
+  LED_G = 0;
+}
+
+int main ( void )
+{
+
+  float rps       = 0.0;
+  float intensity = 0.0;
+
+  set_output_pins();
+  timer0_init();
+  counter1_pwm_init();                       // Enable PWM.
+  int0_pin_change_init();                    // Enable INT0.
+  sei();                                     // Enable interrupts.
+
+  startup_routine();
+
   for (;;)
   {
-    //read_fake_voltage(&test);
+    if (running && tick) {
+      cli();                                  // Stop interrupts.
 
-    adc_value = ADCW;
+      if ( clock == MAX_CLOCK && tick == 0 )
+      {
+        running = 0;
+        LED_G   = 0;
+        LED_R   = 0;
+      }
+      else
+      {
 
-    if (adc_value < POWER_MAX - SHIFT_AT) {
-      power_to_luminance(adc_value, &rgb); // Get intensities
-      set_pwm_intensities(&rgb);           // Set intensities
+        rps = MAX_CLOCK / (clock / tick); // Munge out RPS.
 
-      _delay_us(1024);
-    }
-    else
-    {
-      toggle = !toggle;     // Invert toggle
-      set_flash(toggle); // Set flash
+        if ( rps >= REDLINE )
+        {
+          rps = 141; // Prevent overflows with over revving.
+        }
 
-      _delay_ms(32);
+        intensity = INTENSITY * (rps / 141.0);  // Munge out intensity.
+        set_intensity((uint8_t) intensity);     // Set visual RPM.
+      }
+
+      clock     = 0;                          // Reset clock.
+      tick      = 0;                          // Reset tick.
+
+      sei();                                  // Enable interrupts.
+      _delay_us(10);
     }
 
   }
 
   return 0;
-}
-
-void set_flash ( int toggle )
-{
-  PWM_R  = toggle ? MAX_LUM : 0;
-  PWM_G  = 0;
-}
-
-void set_pwm_intensities ( ColorRGB * __rgb )
-{
-  // Assign tach RGB intensities.
-  PWM_R = __rgb->red;
-  PWM_G = __rgb->green;
-}
-
-void run_startup_sequence ( ColorRGB * _initRGB )
-{
-  int      direction = ASC;
-  uint16_t sweep     = 1;
-
-  for (;;)
-  {
-    if ( sweep >= POWER_MAX )
-    {
-      direction = DESC;
-    }
-
-    sweep += direction;
-
-    if ( sweep < POWER_MIN )
-    {
-      break;
-    }
-
-    power_to_luminance(sweep, _initRGB);
-
-    PWM_R = _initRGB->red;
-    PWM_G = _initRGB->green;
-
-    _delay_us(1024);
-  }
-}
-
-uint16_t power_for ( uint16_t power )
-{
-  float    power_float;
-  uint16_t voltage;
-
-  power_float = (float) power;
-  voltage     = floor(TOTAL_RANGE * ( power_float / POWER_MAX ));
-
-  return voltage;
-}
-
-void power_to_luminance ( uint16_t power , ColorRGB * _rgb )
-{
-  int           remaining = -1;
-  uint16_t      revs      = power_for(power);
-  unsigned int  values[3] = {0,0,0};
-
-  while (remaining != 0)
-  {
-    remaining = revs / MAX_LUM;
-
-    if (remaining > 0)
-    {
-      values[0] = 255;
-    }
-
-    if (remaining > 1)
-    {
-      values[1] = 255;
-    }
-
-    revs = revs % MAX_LUM;
-  }
-
-  if (values[0] && values[1] && !values[2])
-  {
-    values[2] = revs;
-  }
-
-  if (values[0] && !values[1] && !values[2])
-  {
-    values[1] = revs;
-    values[2] = 0;
-  }
-
-  if (!values[0] && !values[1] && !values[2])
-  {
-    values[0] = revs;
-    values[1] = values[2] = 0;
-  }
-
-  if (values[1] > 0)
-  {
-    values[0] -= values[1];
-  }
-
-  if (values[2] > 0) {
-    values[1] -= values[2];
-  }
-
-  _rgb->green = values[1];
-  _rgb->red   = values[2];
 }
